53bc69545fa479a886ba00e8ef532410d8820da7
tano <itytano00@gmail.com>
2023-08-28 15:52:51 +0200

    mandare_File

--- Express/index.js
+++ Express/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
 
 app.listen(8080);
 app.get('/', (req,res)=>{
     
-    res.send("goodbye world! <br> <a href='/tano'> Tano </a>")
-    res.end();
+    res.sendFile('homepage.html', {root: __dirname + '/public'})
+   
 });
-
+//mi secco a rifare il procedimento per le altre pagine tanto ho capito 
+//lo rifarò soltanto se servirà 
 app.get('/tano', (req,res)=>{
         res.send("<h1> un malo sticchione <br> @no__niente<br> <a href='/'> Home </a> </h1>")
         res.end()
@@ -19,4 +21,3 @@ app.all('*',(req,res)=> {
         res.send("<h1>Risorsa non trovata  <a href='/'> Home </a> </h1>")
         res.end();
     });
-//funzionamento uguale al moduloHTTP che tempo sprecato 
