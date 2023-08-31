35ecdcbb5614f211cc2f534dee180002fd8eef87
tano <itytano00@gmail.com>
2023-08-31 17:02:01 +0200

    Query String Params, limit

--- Express/index.js
+++ Express/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-
+const {persone} = require('./persone');
 app.listen(8080);
 app.get('/', (req,res)=>{
     
@@ -14,6 +14,45 @@ app.get('/tano', (req,res)=>{
         res.send("<h1> un malo sticchione <br> @no__niente<br> <a href='/'> Home </a> </h1>")
         res.end()
 
+})
+app.get('/provaJson', (req, res)=>{
+       
+        const nuovapersona = persone.map((persona)=>{
+            const {nome,eta,tabaccoPreferito} = persona;
+            return {nome, eta, tabaccoPreferito};
+    })
+    
+    res.json(nuovapersona);
+
+})
+app.get('/profilo/:id',(req,res)=>{
+    const {id} = req.params
+    const persona = persone.find((personu) => 
+    personu.id===id
+     )
+    if (!persona){
+        return res.status(404).send("persona non esistente");
+    }
+     console.log(persona)
+    res.json(persona);
+    
+})
+app.get('/persone/search', (req,res) =>{
+    console.log(req.query);
+    const {query, limit} = req.query;
+    let Filtrate = [...persone];
+    if (query){
+        Filtrate = Filtrate.filter((persone) => {
+            return persone.nome.startsWith(query.toLowerCase());
+        })
+
+    }
+    if(limit){
+        Filtrate=Filtrate.slice(0,Number(limit));
+    }
+    if(Filtrate.length===0) return res.status(404).send("Persona non trovata");
+    else res.status(200).json(Filtrate);
+    
 })
 
 
---
+++ Express/persone.js
@@ -0,0 +1,41 @@
+const persone = [{
+    id:'1',
+    nome: "tano",
+    eta: 17,
+    tabaccoPreferito: "lucky strike rosso"},
+    {
+        id:"2",
+        nome: "giovanni",
+        eta:18,
+        fuma: true,
+        tabaccoPreferito: "marlboro gold"  
+    },
+{
+    id:'3',
+    nome:'andrea',
+    eta:18,
+    fuma : true,
+    tabaccoPreferito: 'No Name Black'
+},
+{
+    id:'4',
+    nome:'emmanuel',
+    eta:16,
+    fuma : true,
+    tabaccoPreferito: 'non ne ho idea '
+},
+{
+    id:'5',
+    nome:'peppe',
+    eta:18,
+    fuma : false,
+    tabaccoPreferito:'non fuma'
+}, 
+{
+    id:'6',
+    nome:'angela',
+    eta:18,
+    fuma : true,
+    tabaccoPreferito:'winston blu'
+}]
+    module.exports = {persone};
